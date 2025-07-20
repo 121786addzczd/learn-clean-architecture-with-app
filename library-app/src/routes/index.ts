bce60cd3a840f46d962099c8723ebf1995e1f6c4
121786addzczd <1217hs2000ap1@gmail.com>
2025-07-20 13:30:05 +0900

    booksコントローラで作成したルーティング追加

--- library-app/src/routes/index.ts
+++ library-app/src/routes/index.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
+import { BookController } from '../presentation/bookController';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.json({ message: 'Hello world!' });
-});
+const bookController = new BookController();
 
+// ヘルスチェック
 router.get('/health_check', (req, res) => {
   res.json({ message: 'Success Health Check!' });
 });
+
+// Book API
+router.post('/books', bookController.create.bind(bookController));
+router.get('/books/:id', bookController.findById.bind(bookController));
 
 export default router;
