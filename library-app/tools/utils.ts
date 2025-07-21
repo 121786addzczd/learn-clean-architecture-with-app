e15c907720727424d2bfc7de2964923f543fc494
121786addzczd <1217hs2000ap1@gmail.com>
2025-07-21 12:57:34 +0900

    共通関数作成

---
+++ library-app/tools/utils.ts
@@ -0,0 +1,33 @@
+import fs from 'fs';
+import path from 'path';
+
+/**
+  * @description 文字列の先頭を大文字に変換する関数
+  * @param { string } s 変換対象の文字列
+  * @returns { string } 先頭が大文字に変換された文字
+  */
+export function capitalize(s: string): string {
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
+/**
+  * @description 文字列の先頭を小文字に変換する関数
+  * @param { string } s 変換対象の文字列
+  * @returns { string } 先頭が小文字に変換された文字
+  */
+export function lowerCaseFirst(s: string): string {
+  return s.charAt(0).toLowerCase() + s.slice(1);
+}
+
+/**
+ * @description ファイルを書き込む関数
+ * @param { string } filePath ファイルのパス
+ * @param { string } content 書き込む内容
+ */
+export function writeFile(filePath: string, content: string): void {
+  const dir = path.dirname(filePath);
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+  fs.writeFileSync(filePath, content, 'utf8');
+}
